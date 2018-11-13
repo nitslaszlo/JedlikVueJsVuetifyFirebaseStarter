a652896f64b30b5d35cbb702210c33ec7437fbee
nitslaszlo@gmail.com <nitslaszlo@gmail.com>
2018-11-13 08:42:58 +0100

    up

--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,11 @@ import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
 import Vuetify from "vuetify";
+import VueFire from "vuefire";
 import "vuetify/dist/vuetify.min.css";
 
 Vue.use(Vuetify, {});
+Vue.use(VueFire);
 
 Vue.config.productionTip = false;
 
